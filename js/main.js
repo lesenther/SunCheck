31e5fcc0f09570ab828c40c6cf7832d7647931f9
Logan <lobo@MacBook-Air.local>
2015-02-05 15:53:39 -0800

    init hooked to onload and first geolocation code implemented

--- js/main.js
+++ js/main.js
@@ -1,1 +1,64 @@
 
+/**
+ * Initialize the weather app
+ *  - Try to get users location and show current weather data
+ *
+ * @return {[type]} [description]
+ */
+function initialize(){
+  getUserLocation();
+}
+
+/**
+ * Attempt to determine users location using h5 geolocation
+ *  - If successful, put result in input box automatically, and show current
+ *    weather conditions
+ *  - Otherwise, automatically set cursor focus to search box
+ *     - Consider using IP geolocation service as backup
+ *
+ * Uses Googles Geocoding API to translate GPS coords into city, state
+ * See:  https://developers.google.com/maps/documentation/geocoding/
+ *
+ * @return {[type]} [description]
+ */
+function getUserLocation(){
+  if (navigator.geolocation) {
+    var geocoder = new google.maps.Geocoder();
+    navigator.geolocation.getCurrentPosition(function(pos){
+      //alert("determined your position to be " + pos.coords.latitude + ", " + pos.coords.longitude);
+      // Have lat + long, need to make user friendly, like city, state
+      var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (results[0]) {
+            console.log(results);
+            document.getElementById('locationQuery').value = '';
+            getCurrentWeather();
+          } else {
+            alertUser("No results found", 2000);
+            return;
+          }
+        } else {
+          alertUser("Geocoder failed due to: " + status, 2000);
+          return;
+        }
+      });
+    });
+  } else {
+    alert('Geolocation is not supported by this browser.');
+    document.getElementById('locationQuery').focus();
+  }
+}
+
+
+/**
+ * Gets current weather for a location
+ * - Use forecast.io api
+ *
+ * @param  {[type]} lat Latitude
+ * @param  {[type]} lng Longitude
+ * @return {[type]}     [description]
+ */
+function getCurrentWeather(lat, lng){
+  // Perform ajax request and paste info after
+}
