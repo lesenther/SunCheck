ce8bcddf363e3d9300096d12cb386725670ea970
Logan <lobo@MacBook-Air.local>
2015-02-06 21:07:53 -0800

    historical lookup working, added u js obj for config

--- api/index.php
+++ api/index.php
@@ -1,63 +1,73 @@
 <?php
 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 /**
  * SunCheck API for forecast.io
  *
  * For more info, see:
  *  - https://developer.forecast.io/docs/v2
- *  - https://github.com/tobias-redmann/forecast.io-php-api
  *
  **/
 
-include('lib/forecast.io.php');
-
-$api_key = '5f0b624e922d6c1082480617cc2a3767';  // Reset key if compromised!!!
-
+$apiKey = '5f0b624e922d6c1082480617cc2a3767';  // Reset key if compromised!!!
+$request = isset($_GET['request']) ? $_GET['request'] : false;
 $latitude = isset($_GET['latitude']) ? $_GET['latitude'] : false;
 $longitude = isset($_GET['longitude']) ? $_GET['longitude'] : false;
 $locationQuery = isset($_GET['locationQuery']) ? $_GET['locationQuery'] : false;
-$request = isset($_GET['request']) ? $_GET['request'] : false;
-$dateStart = isset($_GET['dateStart']) ? $_GET['dateStart'] : false;
-$dateEnd = isset($_GET['dateEnd']) ? $_GET['dateEnd'] : false;
-$units = 'us';  // Can be set to 'us', 'si', 'ca', 'uk' or 'auto' (see forecast.io API); default is auto
-$lang = ''; // Can be set to 'en', 'de', 'pl', 'es', 'fr', 'it', 'tet' or 'x-pig-latin' (see forecast.io API); default is 'en'
+$dateStart = isset($_GET['dateStart']) ? urldecode($_GET['dateStart']) : false;
+$dateEnd = isset($_GET['dateEnd']) ? urldecode($_GET['dateEnd']) : false;
 
 if(!$request || !$latitude || !$longitude){
   die('Invalid parameters:  ('.$request.' - '.$latitude.', '.$longitude.')');
 }
 
-$forecast = new ForecastIO($api_key);
-
-
 switch ($request) {
   case 'current':
-    $condition = $forecast->getCurrentConditions($latitude, $longitude, $units, $lang);
-    echo json_encode(
-      array(
-        'temp_current' => $condition->getTemperature(),
-        'location_query' => $locationQuery,
-        'temp_apparent' => $condition->getApparentTemperature(),
-        'cloud_cover' => $condition->getCloudCover(),
-        'dew_point' => $condition->getDewPoint(),
-        'humidity' => $condition->getHumidity(),
-        'icon' => $condition->getIcon(),
-        'temp_max' => $condition->getMaxTemperature(),
-        'temp_min' => $condition->getMinTemperature(),
-        'precip_prob' => $condition->getPrecipitationProbability(),
-        'precip_type' => $condition->getPrecipitationType(),
-        'pressure' => $condition->getPressure(),
-        'summary' => $condition->getSummary()
-      )
-    );//*/
+    $weather = json_decode(file_get_contents('https://api.forecast.io/forecast/'.$apiKey.'/'.$latitude.','.$longitude.'?units=us&lang=en'));
+    $currentWeather = $weather->currently;
+    $currentWeather->location_query = $locationQuery;
+    echo json_encode($currentWeather);
     break;
 
   case 'historical':
-    echo 'not yet implemented';
+    $dateRange = new DatePeriod(
+      new DateTime($dateStart),
+      new DateInterval('P1D'),
+      new DateTime($dateEnd)
+    );
+
+    $dayCount=0;
+    $tempMap='';
+    $rainMap='';
+    $daysSunny=0;
+    foreach($dateRange as $date){
+      $weather = json_decode(file_get_contents('https://api.forecast.io/forecast/'.$apiKey.'/'.$latitude.','.$longitude.','.$date->getTimestamp().'?units=us&lang=en&exclude=minutely,hourly,currently'));
+      $dailyWeather = $weather->daily->data[0];
+      if($dailyWeather->icon=='clear-day'){
+        $daysSunny++;
+      }
+      $dayCount++;
+      $averageTemperature = $dailyWeather->temperatureMin + ($dailyWeather->temperatureMax - $dailyWeather->temperatureMin)/2;
+      $tempMap .= '<span style="background-color:'.($averageTemperature).';">&nbsp;</span>';
+      $rainMap .= '<span style="background-color:rgb('.($dailyWeather->icon == 'rain' ? '100,100,255' : '255,255,255').');">&nbsp;</span>';
+    }
+    $percentSunny = number_format($daysSunny/$dayCount*100).'%';
+
+    echo json_encode(
+      array(
+        'location_query' => $locationQuery,
+        'total_days' => $dayCount,
+        'average_temperature' => $averageTemperature,
+        'percent_sunny' => $percentSunny,
+        'temp_map' => $tempMap,
+        'rain_map' => $rainMap,
+        'other' => 'bleh'
+      )
+    );
     break;
-
-  //case 'locationCoordinates':
-    //https://maps.googleapis.com/maps/api/geocode/json?address=Toledo&key=API_KEY
-
 
   default:
     echo 'Bad request:  '.$request;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var u = {
+  query: '',
+  lat: '',
+  lng: '',
+  dstart: '1/2/13',
+  dend: '1-8-2013'
+};
+
 /**
  * Initialize the weather app
  *  - Try to get users location and show current weather data
@@ -6,6 +14,7 @@
  */
 function initialize(){
   getUserLocation();
+  return;
 }
 
 
@@ -22,38 +31,46 @@ function initialize(){
  * @return {[type]} [description]
  */
 function getUserLocation(){
+  u.query = document.getElementById('locationQuery').value;
   if (navigator.geolocation) {
-    alertUser('<span class="load"></span>Trying to determine your location..', 99999);
+    alertUser('<span class="load"></span>Determining your location', 99999);
     var geocoder = new google.maps.Geocoder();
     navigator.geolocation.getCurrentPosition(function(pos){
-      var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+      u.lat = pos.coords.latitude;
+      u.lng = pos.coords.longitude;
+      var latlng = new google.maps.LatLng(u.lat, u.lng);
       geocoder.geocode({'latLng': latlng}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
           if (results[0]) {
-            var city = 'unknown';
-            var state = 'unknown';
-             var arrAddress = results[0].address_components;
-              for (ac = 0; ac < arrAddress.length; ac++) {
-                if (arrAddress[ac].types[0] == "locality") { city = arrAddress[ac].long_name }
-                if (arrAddress[ac].types[0] == "administrative_area_level_1") { state = arrAddress[ac].short_name }
-              }
-            document.getElementById('locationQuery').value = city + ', ' + state;
-            document.getElementById('mask').style.display = 'none';
-            getCurrentWeather(pos.coords.latitude,pos.coords.longitude);
+            var city = 'unknown', state = 'unknown';
+            var arrAddress = results[0].address_components;
+            for (ac = 0; ac < arrAddress.length; ac++) {
+              city = (arrAddress[ac].types[0] == "locality")
+                ? arrAddress[ac].long_name : city;
+              state = (arrAddress[ac].types[0] == "administrative_area_level_1")
+                ? arrAddress[ac].short_name : state;
+            }
+            u.query = city + ', ' + state;
+            document.getElementById('locationQuery').value = u.query;
+            hideAlert();
+            getCurrentWeather();
           } else {
-            alertUser("No results found");
+            alertUser("Location could not be found");
+            document.getElementById('locationQuery').select();
             return;
           }
         } else {
-          alertUser("Geocoder failed due to: " + status);
+          alertUser("Geocoder failed: <em>" + status + '</em>');
+            document.getElementById('locationQuery').select();
           return;
         }
       });
     });
   } else {
     alertUser('Geolocation is not supported by this browser.');
-    document.getElementById('locationQuery').focus();
+    document.getElementById('locationQuery').select();
   }
+  return;
 }
 
 
@@ -62,68 +79,112 @@ function getUserLocation(){
  * - Use forecast.io api
  *
  * - Needs latitude and longitude!
+ * - Response is in json format
  *
- * @param  {[type]} lat Latitude
- * @param  {[type]} lng Longitude
  * @return {[type]}     [description]
  */
-function getCurrentWeather(lat, lng){
-  if (!lat || !lng){
-    alertUser('Bad input parameters');
+function getCurrentWeather(){
+  u.query = document.getElementById('locationQuery').value;
+  if( !u.lat || !u.lng)
     return;
-  }
-  alertUser('<span class="load"></span>Getting weather data', 99999);
+  alertUser('<span class="load"></span>Getting weather data for <em>' +
+    u.query + '</em>', 99999);
   $.ajax({
     type: 'GET',
     url: '/api',
     data:{
       request: 'current',
-      locationQuery: document.getElementById('locationQuery').value,
-      latitude: lat,
-      longitude: lng
+      locationQuery: u.query,
+      latitude: u.lat,
+      longitude: u.lng
     },
     dataType: "json"
-  }).done(function(msg){
-    document.getElementById('mask').style.display = 'none';
+  }).done(function(data){
+    hideAlert();
     document.getElementById('results').innerHTML =
-      '<strong>Weather right now in ' + msg.location_query + ':</strong>' +
+      '<p>Weather right now in <em>' + u.query + '</em>:</p>' +
       '<ul>' +
-        '<li>Conditions:  <strong>' + msg.summary + '</strong></li>' +
-        '<li>Temperature: <strong>' + msg.temp_current + ' &deg; F</strong></li>' +
+        '<li><strong>' + data.summary + '</strong></li>' +
+        '<li class="temp">' + Math.floor(data.temperature) + ' &deg;F</li>' +
+        '<li>' + (data.humidity*100) + '% Humidity</li>' +
       '</ul>' +
-      '<button onclick="getHistorialWeather();">Get Historical Data</button>';
+      '<div style="text-align:center;">' +
+        '<a onclick="getHistorialWeather();">' +
+          'Get Historical Data' +
+        '</a>' +
+      '</div>';
+    document.getElementById('locationQuery').select();
   });
+  return;
 }
 
 
 /**
  * Gets weather data for a specified location across a range of dates
  *
- * @param  {[type]} lat [description]
- * @param  {[type]} lng [description]
  * @return {[type]}     [description]
  */
-function getHistorialWeather(lat, lng){
-  var dateStart = '', dateEnd = '';
-  while(!isValidDate(dateStart)){
-    dateStart = Date.parse(prompt('Start date:  ', 'MM/DD/YYYY'));
-  }
-  if (dateStart=='')
+function getHistorialWeather(){
+  var dateStart = new Date(Date.parse(prompt('Enter the start date:  \n\n' +
+    '(MM-DD-YY or any format you wish)', u.dstart)));
+  if (dateStart=='' || !isValidDate(dateStart)){
+    alertUser('<strong>Error:</strong> Bad start date format');
     return;
-  while(!isValidDate(dateEnd)){
-    dateEnd = Date.parse(prompt('End date:  ', 'MM/DD/YYYY'));
   }
-  if (dateEnd=='')
+  var dateEnd = new Date(Date.parse(prompt('Enter the end date:  \n\n' +
+    '(MM-DD-YY or any format you wish)', u.dend)));
+  if (dateEnd=='' || !isValidDate(dateEnd)){
+    alertUser('<strong>Error:</strong> Bad end date format');
     return;
-  document.getElementById('results').innerHTML =
-    '<strong>Stats from ' + dateStart + ' to ' + dateEnd + ' in ' + document.getElementById('locationQuery').value + ':</strong>' +
-    '<ul>' +
-      '<li>Sunny Days in range: <strong>82%</strong></li>' +
-      '<li>Temperature Map:  <div id="temperatureMap"></div></li>' +
-      '<li>Rainfall Map:  <div id="rainFallMap"></div></li>' +
-      '<li>...</li>' +
-    '</ul>';
+  }
+  if(dateStart > dateEnd){ // Swap dates if the user enters them backwards
+    var temp = dateStart;
+    dateStart = dateEnd;
+    dateEnd = temp;
+  }
+  u.dstart = properDate(dateStart);
+  u.dend = properDate(dateEnd);
+  // After basic validation, make ajax request
+  alertUser('<span class="load"></span>Getting weather data for <em>' +
+    document.getElementById('locationQuery').value + '</em>', 99999);
+  $.ajax({
+    type: 'GET',
+    url: '/api',
+    data:{
+      request: 'historical',
+      locationQuery: u.query,
+      dateStart: u.dstart,
+      dateEnd: u.dend,
+      latitude: u.lat,
+      longitude: u.lng
+    },
+    dataType: "json"
+  }).done(function(data){
+    hideAlert();
+    document.getElementById('results').innerHTML =
+      '<p>Weather between <em>' + u.dstart + '</em> and <em>' +
+        u.dend + '</em> in <em>' + u.query + '</em>:</p>' +
+      '<ul>' +
+        '<li><strong>' + data.percent_sunny + '</strong> of the days during ' +
+        'this period were sunny</li>' +
+        '<li>Temperature Map:  <br>' +
+          '<div id="temperatureMap"><img src="' + data.temp_map + '"></div>' +
+        '</li>' +
+        '<li>Rainfall Map:  <br>' +
+          '<div id="rainFallMap"><img src="' + data.rain_map + '"></div>' +
+        '</li>' +
+        '<li>...</li>' +
+      '</ul>' +
+      '<div style="text-align:center;">' +
+        '<a onclick="getHistorialWeather();">' +
+          'Get Historical Data' +
+        '</a>' +
+      '</div>';
+      document.getElementById('locationQuery').select();
+  });
+  return;
 }
+
 
 /**
  * Use Google geocoding to get coordinates from users query
@@ -131,35 +192,42 @@ function getHistorialWeather(lat, lng){
  * @param  {[type]} locationQuery [description]
  * @return {[type]}               [description]
  */
-function findCoordsForLocation(locationQuery){
-  if (!locationQuery){
-    alertUser('<strong>Error:</strong> Bad input parameter:  ' + locationQuery);
+function findCoordsForLocation(){
+  u.query = document.getElementById('locationQuery').value;
+  if (!u.query){
+    alertUser('<strong>Error:</strong> Bad input parameter <em>' +
+      u.query + '</em>');
     return;
   }
-  alertUser('<span class="load"></span>Getting location coordinates', 99999);
+  alertUser('<span class="load"></span>Getting coordinates for <em>' +
+    u.query + '</em>', 99999);
   $.ajax({
     type: 'GET',
     url: 'https://maps.googleapis.com/maps/api/geocode/json',
     data:{
-      address: locationQuery
+      address: u.query
     },
     dataType: "json"
-  }).done(function(msg){
-    document.getElementById('mask').style.display = 'none';
-    if (msg.status == 'OK'){
-      document.getElementById('locationQuery').value = msg.results[0].formatted_address;
-      getCurrentWeather(msg.results[0].geometry.location.lat,msg.results[0].geometry.location.lng)
+  }).done(function(data){
+    hideAlert();
+    if (data.status == 'OK'){
+      u.query = data.results[0].formatted_address;
+      document.getElementById('locationQuery').value = u.query;
+      getCurrentWeather()
     }else{
-      alertUser('<strong>Error:</strong> Location not found:  <em>' + locationQuery + '</em>');
+      alertUser('<strong>Error:</strong> Location not found <em>' +
+        u.query + '</em>');
     }
   });
+  return;
 }
 
 
 /**
  * Checks for valid date object
  *
- * Jacked from:  http://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript
+ * Credit:
+ * http://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript
  *
  * @param  {[type]}  d [description]
  * @return {Boolean}   [description]
@@ -172,7 +240,18 @@ function isValidDate(d) {
 
 
 /**
- * Informs user whats happening
+ * Takes a date object and returns a string in MM/DD/YYYY format
+ *
+ * @param  {[type]} d [description]
+ * @return {[type]}   [description]
+ */
+function properDate(d) {
+  return d.getMonth()+1 + "/" + d.getDate() + "/" + d.getFullYear();
+}
+
+
+/**
+ * Temporarily display a message to the user over the application
  *
  * @param  {[type]} msg     [description]
  * @param  {[type]} timeout [description]
@@ -183,6 +262,18 @@ function alertUser(msg, timeout){
   document.getElementById('notice').innerHTML = msg;
   document.getElementById('mask').style.display = 'block';
   setTimeout(function(){
-    document.getElementById('mask').style.display = 'none';
+    hideAlert();
   }, timeout);
+  return;
+}
+
+
+/**
+ * Hides the user alert
+ *
+ * @return {[type]} [description]
+ */
+function hideAlert(){
+  document.getElementById('mask').style.display = 'none';
+  return;
 }
